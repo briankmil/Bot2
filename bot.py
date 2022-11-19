e4227298a693659bfb722acdd0a5a4025a70f765
Brian Piragauta <bpiragauta@skgtecnologia.com>
2022-11-19 10:38:48 -0500

    Se agrega el comando /imc

--- bot.py
+++ bot.py
@@ -3,10 +3,34 @@ from config import bot
 import config
 from time import sleep
 import re
+from telebot import types
 #########################################################
 
 
 # Aquí vendrá la implementación de la lógica del bot
+
+bot_data = {}
+class Record:
+    def __init__(self):
+        self.height = None
+        self.weight = None
+        self.gender = None
+
+class Switch(dict):
+    def __getitem__(self, item):
+        for key in self.keys():                 # iterate over the intervals
+            if item in key:                     # if the argument is in that interval
+                return super().__getitem__(key) # return its associated value
+        raise KeyError(item)                    # if not in any interval, raise KeyError
+
+# Enable saving next step handlers to file "./.handlers-saves/step.save".
+# Delay=2 means that after any change in next step handlers (e.g. calling register_next_step_handler())
+# saving will hapen after delay 2 seconds.
+bot.enable_save_next_step_handlers(delay=2)
+# Load next_step_handlers from save file (default "./.handlers-saves/step.save")
+# WARNING It will work only if enable_save_next_step_handlers was called!
+bot.load_next_step_handlers()
+
 
 
 @bot.message_handler(commands=['start'])
@@ -19,45 +43,85 @@ def on_command_start(message):
         parse_mode="Markdown")
 
 
-@bot.message_handler(regexp=r"^sumar ([+-]?([0-9]*[.])?[0-9]+) y ([+-]?([0-9]*[.])?[0-9]+)$")
-def on_add(message):
+@bot.message_handler(commands=['help'])
+def on_command_help(message):
     bot.send_chat_action(message.chat.id, 'typing')
     sleep(1)
-    parts = re.match(
-        r"^sumar ([+-]?([0-9]*[.])?[0-9]+) y ([+-]?([0-9]*[.])?[0-9]+)$",
-        message.text,
-        flags=re.IGNORECASE)
-    # print (parts.groups())
-    oper1 = float(parts[1])
-    oper2 = float(parts[3])
-    result = oper1 + oper2
-    bot.reply_to(
-        message,
-        f"\U0001F522 Resultado: {result}")
+    response = ("Estos son los comandos y órdenes disponibles:\n"
+    "\n"
+    "*/start* - Inicia la interacción con el bot\n"
+    "*/help* - Muestra este mensaje de ayuda\n"
+    "*/imc* - Inicia el proceso de cálculo del Índice de Masa Corporal\n"
+    )
+    bot.send_message(
+    message.chat.id,
+    response,
+    parse_mode="Markdown")
 
+@bot.message_handler(commands=['menu'])
+def on_command_menu(message):
+    # Using the ReplyKeyboardMarkup class
+    # It's constructor can take the following optional arguments:
+    # - resize_keyboard: True/False (default False)
+    # - one_time_keyboard: True/False (default False)
+    # - selective: True/False (default False)
+    # - row_width: integer (default 3)
+    # row_width is used in combination with the add() function.
+    # It defines how many buttons are fit on each row before continuing on the next row.
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    itembtn1 = types.KeyboardButton('/imc')
+    itembtn2 = types.KeyboardButton('/help')
+    markup.add(itembtn1, itembtn2)
+    bot.send_message(message.chat.id, "Selecciona una opción del menú:",
+        reply_markup=markup)
 
-@bot.message_handler(regexp=r"^dividir ([+-]?([0-9]*[.])?[0-9]+) y ([+-]?([0-9]*[.])?[0-9]+)$")
-def on_add(message):
-    bot.send_chat_action(message.chat.id, 'typing')
-    sleep(1)
-    parts = re.match(
-        r"^dividir ([+-]?([0-9]*[.])?[0-9]+) y ([+-]?([0-9]*[.])?[0-9]+)$",
-        message.text,
-        flags=re.IGNORECASE)
-    # print (parts.groups())
-    oper1 = float(parts[1])
-    oper2 = float(parts[3])
-    if oper2 == 0:
-        bot.reply_to(
-        message,
-        f"\U0001F62D No puedo realizar esta operación")
-    else:
-        result = oper1 / oper2
-        bot.reply_to(
-        message,
-        f"\U0001F522 Resultado: {result}")
-         
+@bot.message_handler(commands=['imc'])
+def on_command_imc(message):
+    response = bot.reply_to(message, "¿Cuál es tu estatura en metros?")
+    bot.register_next_step_handler(response, process_height_step)
 
+def process_height_step(message):
+    try:
+        height = float(message.text)
+        record = Record()
+        record.height = height
+        bot_data[message.chat.id] = record
+        response = bot.reply_to(message, '¿Cuál es tu peso en kilogramos?')
+        bot.register_next_step_handler(response, process_weight_step)
+    except Exception as e:
+        bot.reply_to(message, f"Algo terrible sucedió: {e}")
+
+def process_weight_step(message):
+    try:
+        weight = float(message.text)
+        record = bot_data[message.chat.id]
+        record.weight = weight
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add('Male', 'Female')
+        response = bot.reply_to(message, '¿Cuál es tu género?',
+        reply_markup=markup)
+        bot.register_next_step_handler(response, process_gender_step)
+    except Exception as e:
+        bot.reply_to(message, f"Algo terrible sucedió: {e}")
+
+def process_gender_step(message):
+    gender = message.text
+    record = bot_data[message.chat.id]
+    record.gender = gender
+    imc(message)
+
+def imc(message):
+    record = bot_data[message.chat.id]
+    imc = record.weight / pow(record.height, 2)
+    switcher= Switch({
+        range(0, 18):'Peso bajo',
+        range(18, 24):'Normal',
+        range(25, 29):'Sobrepeso',
+        range(30, 49):'Obesidad'
+    })
+    
+    answer = f"Data = (Height: {record.height}, Weight: {record.weight}, Gender: {record.gender})\nIMC = {imc}\nResultado: {switcher[int(imc)]}"
+    bot.reply_to(message, answer)
 
 @bot.message_handler(func=lambda message: True)
 def on_fallback(message):
